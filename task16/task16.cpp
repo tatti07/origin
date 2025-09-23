cb14f526092c5b39698d19863a9be15b7a9ddbc4
Iakovleva Tatiana <tatianaiakovleva07@gmail.com>
2025-09-23 19:30:15 +0900

    Задание 16

---
+++ task16/task16.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+#include <io.h>
+#include <fcntl.h>
+
+struct bad_length {};
+
+int main() {
+    _setmode(_fileno(stdout), _O_U16TEXT);
+    _setmode(_fileno(stdin), _O_U16TEXT);
+
+    int forbidden = 0;
+    std::wcout << L"Введите запрещённую длину: ";
+    std::wcin >> forbidden;
+
+    while (true) {
+        std::wcout << L"Введите слово: ";
+        std::wstring word;
+        if (!(std::wcin >> word)) break;
+
+        try {
+            int len = static_cast<int>(word.size());
+            if (len == forbidden) throw bad_length{};
+            std::wcout << L"Длина слова \"" << word << L"\" равна " << len << L"\n";
+        }
+        catch (const bad_length&) {
+            std::wcout << L"Вы ввели слово запрещённой длины! До свидания\n";
+            break;
+        }
+    }
+    return 0;
+}
