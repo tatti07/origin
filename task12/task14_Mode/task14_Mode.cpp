5fd48116d7e95e0ad837364cff2a112574a55f85
Iakovleva Tatiana <tatianaiakovleva07@gmail.com>
2025-09-23 13:44:13 +0900

    Задание 14

---
+++ task12/task14_Mode/task14_Mode.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+
+
+#define MODE 1
+
+#ifndef MODE
+#  error "Необходимо определить MODE"
+#endif
+
+#if MODE == 1
+int add(int x, int y) { return x + y; }
+#endif
+
+int main() {
+#if MODE == 0
+    
+    std::cout << "Работаю в режиме тренировки\n";
+
+#elif MODE == 1
+    
+    std::cout << "Работаю в боевом режиме\n";
+    int a{}, b{};
+    std::cout << "Введите число 1: ";
+    std::cin >> a;
+    std::cout << "Введите число 2: ";
+    std::cin >> b;
+    std::cout << "Результат сложения: " << add(a, b) << '\n';
+
+#else
+
+    std::cout << "Неизвестный режим. Завершение работы\n";
+#endif
+
+    return 0;
+}
