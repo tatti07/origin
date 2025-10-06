e3181de64fe8c9bf5cee8a76fa7e4ff265833738
Iakovleva Tatiana <tatianaiakovleva07@gmail.com>
2025-10-06 20:58:35 +0300

    CMake

---
+++ my_first_cmake_project/my_first_cmake_project.cpp
@@ -0,0 +1,9 @@
+﻿#include <iostream>
+#include <string>
+int main() {
+    std::cout << "Enter your name: ";
+    std::string name;
+    std::cin >> name;
+    std::cout << "Hello, " << name << "!\n";
+    return 0;
+}
